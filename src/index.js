cb8e0a411bc081f0b501c3d7868fbb40892b5e2c
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-05-18 22:03:57 +0200

    get movies from db

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 //5. Importo con require el movies.json en src/index.js
-const dataMovies = require("./data/movies.json");
+//const dataMovies = require("./data/movies.json");
 const users = require('./data/users.json');
 const Database = require('better-sqlite3');
 const { response } = require("express");
@@ -30,28 +30,31 @@ server.get("/movies", (req, res) => {
   const query = db.prepare('SELECT * FROM movies');
   const movies = query.all();
   console.log(movies);
-  res.json(movies);
+  return res.json({
+    success: true,
+    movies: movies
+  });
 });
   //6. como ya lo tengo importado, dentro de este endpoint que he creado en el punto 3, me retorno las peliculas
   //guardamos el valor del query param de género en una constante
 
-//   const genderFilterParam = req.query.gender;
+  // const genderFilterParam = req.query.gender;
 
-//   //Respondemos con el listado filtrado.
-//   res.json({
-//     success: true,
-//     movies: dataMovies.movies
-//       .filter((item) => item.gender.includes(genderFilterParam))
-//       .sort(function (a, z) {
-//         const sortFilterParam = a.title.localeCompare(z.title);
-//         if (req.query.sort === 'asc') {
-//           return sortFilterParam;
-//         } else {
-//           return sortFilterParam * -1;
-//         }
-//       }),
-//   });
-// });
+  // //Respondemos con el listado filtrado.
+  // res.json({
+  //   success: true,
+  //   movies: dataMovies.movies
+  //     .filter((item) => item.gender.includes(genderFilterParam))
+  //     .sort(function (a, z) {
+  //       const sortFilterParam = a.title.localeCompare(z.title);
+  //       if (req.query.sort === 'asc') {
+  //         return sortFilterParam;
+  //       } else {
+  //         return sortFilterParam * -1;
+  //       }
+  //     }),
+  // });
+
 
 
 server.post("/login", (req, res) => {
